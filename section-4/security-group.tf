5e5638c01269ac571eaeb07b1778d989ffcab09a
Elton Li <eltonyt.li@gmail.com>
2026-01-19 00:15:45 -0600

    Added Reference Practice

--- section-4/security-group.tf
+++ section-4/security-group.tf
@@ -11,6 +11,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   to_port           = 80
 }
 
+# PRACTICE 3 - VARIABLE REFERENCE FROM DIFFERENT MODULE
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "${aws_eip.lb.public_ip}/32"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
